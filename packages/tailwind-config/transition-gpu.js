58039ea9f26a3a5d37509ccf92b281a7708f5f13
Dave Honneffer <pearofducks@gmail.com>
2021-04-27 10:26:36 +0200

    Port early classes from components, WIP

---
+++ packages/tailwind-config/transition-gpu.js
@@ -0,0 +1,8 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(({ addUtilities }) => {
+    const transitionGpu = {
+        ['transition-gpu']: { backfaceVisibility: 'hidden' },
+    };
+    addUtilities(transitionGpu);
+});
