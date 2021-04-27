4c937bbe2aad38e7f8f5f7eba3fcf3bd906eab10
Dave Honneffer <pearofducks@gmail.com>
2021-04-27 10:30:34 +0200

    Fix the reduced-motion util

--- packages/tailwind-config/reduced-motion.js
+++
@@ -1,8 +0,0 @@
-const plugin = require('tailwindcss/plugin');
-
-module.exports = plugin(({ addUtilities, e }) => {
-    const durationZero = {
-        [e('reduced-motion:duration-0')]: { transitionDuration: '0s' },
-    };
-    addUtilities(durationZero);
-});
