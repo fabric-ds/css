39272ccb162cbbaa88bdd1b97009dc851f8be26c
Dave Honneffer <pearofducks@gmail.com>
2021-05-25 15:54:16 +0200

    Port component classes over

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -119,9 +119,13 @@ module.exports = {
         },
         // Effects
         boxShadow: {
-            sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-            DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-            lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+            'light-2': '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+            'light-1': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+            'light-3': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+            DEFAULT: '0 1px 3px rgba(0, 0, 0, .12), 0 1px 2px rgba(0, 0, 0, .24)',
+            2: '0 3px 6px rgba(0, 0, 0, .16), 0 3px 6px rgba(0, 0, 0, .23)',
+            3: '0 10px 20px rgba(0, 0, 0, .19), 0 6px 6px rgba(0, 0, 0, .23)',
+            4: '0 14px 28px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .22)',
             none: 'none',
         },
         screens: {
