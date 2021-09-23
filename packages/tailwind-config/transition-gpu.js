dcef92ba8fd1b6b9a84e76b7d041c098aec2eb8d
Dave Honneffer <pearofducks@gmail.com>
2021-09-23 12:16:49 +0200

    Fix typo

--- packages/tailwind-config/transition-gpu.js
+++ packages/tailwind-config/transition-gpu.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(({ addUtilities }) => {
     const transitionGpu = {
-        ['transition-gpu']: { backfaceVisibility: 'hidden' },
+        ['.transition-gpu']: { backfaceVisibility: 'hidden' },
     };
     addUtilities(transitionGpu);
 });
