745feff0d1ec55f13be5dad03b2a4df6df86ca7d
Dave Honneffer <pearofducks@gmail.com>
2021-10-12 16:46:39 +0200

    Change colors (components still need shifted)

--- packages/tailwind-config/colors.js
+++ packages/tailwind-config/colors.js
@@ -1,127 +1,128 @@
 const colors = {
-    blue: {
-        50: '#eff6ff',
-        100: '#dbeafe',
-        200: '#bfdbfe',
-        300: '#93c5fd',
-        400: '#60a5fa',
-        500: '#3b82f6',
-        600: '#2563eb', // Primary Blue
-        700: '#1d4ed8',
-        800: '#1e40af',
-        900: '#1e3a8a',
-    },
-    aqua: {
-        50: '#f0f9ff', // Ice
-        100: '#e0f2fe',
-        200: '#bae6fd', // Toothpaste
-        300: '#7dd3fc',
-        400: '#38bdf8', // Secondary Blue
-        500: '#0ea5e9',
-        600: '#0284c7',
-        700: '#0369a1',
-        800: '#075985',
-        900: '#0c4a6e',
-    },
-    green: {
-        50: '#ecfdf5',
-        100: '#d1fae5',
-        200: '#a7f3d0', // Mint
-        300: '#6ee7b7',
-        400: '#34d399', // Pea
-        500: '#10b981',
-        600: '#059669', // Lime
-        700: '#047857',
-        800: '#065f46',
-        900: '#064e3b',
-    },
-    yellow: {
-        50: '#fefce8',
-        100: '#fef9c3', // Banana
-        200: '#fef08a',
-        300: '#fde047',
-        400: '#facc15',
-        500: '#eab308',
-        600: '#ca8a04',
-        700: '#a16207',
-        800: '#854d0e',
-        900: '#713f12',
-    },
-    red: {
-        50: '#fef2f2',
-        100: '#fee2e2',
-        200: '#fecaca', // Salmon
-        300: '#fca5a5',
-        400: '#f87171', // Watermelon
-        500: '#ef4444',
-        600: '#dc2626', // Cherry
-        700: '#b91c1c',
-        800: '#991b1b',
-        900: '#7f1d1d',
-    },
-    bluegray: {
-        50: '#f8fafc',
-        100: '#f1f5f9', // Marble
-        200: '#e2e8f0',
-        300: '#cbd5e1', // Sardine
-        400: '#94a3b8',
-        500: '#64748b',
-        600: '#475569',
-        700: '#334155',
-        800: '#1e293b',
-        900: '#0f172a',
-    },
-    gray: {
-        50: '#fafafa',
-        100: '#f5f5f5',
-        200: '#e5e5e5',
-        300: '#d4d4d4',
-        400: '#a3a3a3',
-        500: '#737373', // Stone
-        600: '#525252',
-        700: '#404040', // Licorice
-        800: '#262626',
-        900: '#171717',
-    },
-    white: '#fff',
-    current: 'currentColor',
-    transparent: 'transparent',
+  blue: {
+    50:  "#f2f6fc",
+    100: "#c4dbfe",
+    200: "#97bffe",
+    300: "#69a3fd",
+    400: "#3885fc",
+    500: "#0063fb", // primary
+    600: "#0056db",
+    700: "#004abc",
+    800: "#003e9d",
+    900: "#003280",
+  },
+  aqua: {
+    50:  "#f1f9ff", // ice
+    100: "#d3edfe",
+    200: "#b4e1fe",
+    300: "#90d5fd", // toothpaste
+    400: "#65cafc",
+    500: "#06befb", // secondary
+    600: "#05a4d9",
+    700: "#048bb7",
+    800: "#037297",
+    900: "#025b78",
+  },
+  green: {
+    50: "#eaf6f1",
+    100: "#ccffec", // mint
+    200: "#b4fadc",
+    300: "#a5f9d9",
+    400: "#7df3c6",
+    500: "#2ee69f", // pea
+    600: "#10b981",
+    700: "#059669", // lime
+    800: "#065f46",
+    900: "#064e3b",
+  },
+  yellow: {
+    50: "#faf6eb",
+    100: "#FFF5C8", // banana
+    200: "#FFF0A8",
+    300: "#FEEB88",
+    400: "#FEE567",
+    500: "#f39d2a",
+    600: "#d77720",
+    700: "#b2531a",
+    800: "#914017",
+    900: "#773515",
+  },
+
+  red: {
+    50: "#f7f2f2",
+    100: "#FFEFEF", // salmon
+    200: "#ffd6d1",
+    300: "#feb9b9",
+    400: "#fd9191",
+    500: "#FF5844", // watermelon
+    600: "#e03622",
+    700: "#d9270a", // cherry
+    800: "#ba1a00",
+    900: "#991000",
+  },
+  bluegray: {
+    50: "#F6F8FB", // marble
+    100: "#ECEFF4",
+    200: "#E2E6ED",
+    300: "#D7DEE7",
+    400: "#C3CCD9", // sardine
+    500: "#9FA8B6",
+    600: "#7B8493",
+    700: "#575F70",
+    800: "#333B4D",
+    900: "#0F172A",
+  },
+  gray: {
+    50: "#f6f6f8",
+    100: "#F4F4F5",
+    200: "#E4E4E7",
+    300: "#D4D4D8",
+    400: "#BCBCBF",
+    500: "#A4A4A6",
+    600: "#8D8D8D",
+    700: "#767676", // stone
+    800: "#474445", // licorice
+    900: "#262626",
+  },
+  white: '#fff',
+  current: 'currentColor',
+  transparent: 'transparent',
 };
 
 const textColors = {
-    primary: 'var(--f-gray-700)',
-    secondary: 'var(--f-gray-500)',
-    danger: 'var(--f-red-600)',
+  primary: 'var(--f-gray-800)',
+  secondary: 'var(--f-gray-700)',
+  danger: 'var(--f-red-700)',
 };
 
 const breakpoints = {
-    '--sm': '(min-width: 480px)',
-    '--md': '(min-width: 768px)',
-    '--lg': '(min-width: 990px)',
-    '--max480': '(max-width: 479px)',
-    '--min480': '(min-width: 480px)',
-    '--max768': '(max-width: 767px)',
-    '--min768': '(min-width: 768px)',
-    '--max990': '(max-width: 989px)',
-    '--min990': '(min-width: 990px)',
+  '--sm': '(min-width: 480px)',
+  '--md': '(min-width: 768px)',
+  '--lg': '(min-width: 990px)',
+  '--max480': '(max-width: 479px)',
+  '--min480': '(min-width: 480px)',
+  '--max768': '(max-width: 767px)',
+  '--min768': '(min-width: 768px)',
+  '--max990': '(max-width: 989px)',
+  '--min990': '(min-width: 990px)',
 };
 
 const troikaAliases = {
-    primaryBlue: 'var(--f-blue-600)',
-    secondaryBlue: 'var(--f-aqua-400)',
-    ice: 'var(--f-aqua-50)',
-    toothpaste: 'var(--f-aqua-200)',
-    mint: 'var(--f-green-200)',
-    pea: 'var(--f-green-400)',
-    lime: 'var(--f-green-600)',
-    banana: 'var(--f-yellow-100)',
-    salmon: 'var(--f-red-200)',
-    watermelon: 'var(--f-red-400)',
-    cherry: 'var(--f-red-600)',
-    marble: 'var(--f-bluegray-100)',
-    sardine: 'var(--f-bluegray-300)',
-    stone: 'var(--f-gray-500)',
-    licorice: 'var(--f-gray-700)',
+  primaryBlue: 'var(--f-blue-500)',
+  secondaryBlue: 'var(--f-aqua-500)',
+  ice: 'var(--f-aqua-50)',
+  toothpaste: 'var(--f-aqua-300)',
+  mint: 'var(--f-green-100)',
+  pea: 'var(--f-green-500)',
+  lime: 'var(--f-green-700)',
+  banana: 'var(--f-yellow-100)',
+  salmon: 'var(--f-red-100)',
+  watermelon: 'var(--f-red-500)',
+  cherry: 'var(--f-red-700)',
+  marble: 'var(--f-bluegray-50)',
+  sardine: 'var(--f-bluegray-400)',
+  stone: 'var(--f-gray-700)',
+  licorice: 'var(--f-gray-800)',
 };
 
 module.exports = { colors, textColors, breakpoints, troikaAliases };
