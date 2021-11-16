a0e048b28e099e1ca0178eb54d1fd2c1df5db588
Dave Honneffer <pearofducks@gmail.com>
2021-11-16 16:15:40 +0100

    Fix focus-ring implementation, allow overrides

--- packages/tailwind-config/focus-ring.js
+++ packages/tailwind-config/focus-ring.js
@@ -2,14 +2,19 @@ const plugin = require('tailwindcss/plugin');
 
 const focusRing = {
     outline: '2px solid var(--f-aqua-400)',
-    outlineOffset: '1px',
+    outlineOffset: 'var(--f-outline-offset, 1px)',
 };
+
+const focusRingInset = {
+    '--f-outline-offset': '-2px'
+}
 
 module.exports = plugin(({ addUtilities }) => {
     const focusRingClass = {
         '.focus-ring:focus': focusRing,
         '.focus-ring:focus:not(:focus-visible)': { outline: 'none' },
         '.focus-ring:focus-visible': focusRing,
+        '.focus-ring-inset': focusRingInset,
     };
     addUtilities(focusRingClass);
 });
